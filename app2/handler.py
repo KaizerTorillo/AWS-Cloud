e58a628cb4181892d0892b8e8f57d9f38335638f
Kaizer <kaizertorillo@gmail.com>
2023-12-21 14:48:20 +1300

    aws cloud

--- app2/handler.py
+++ app2/handler.py
@@ -1,24 +1,40 @@
 import json
+import os 
+import sys
+import uuid
+from urllib.parse import unquote_plus
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+#   def create_thumbnail(event, context):
+#       body = {
+#        "message": "Go Serverless v1.0! Your function executed successfully!",
+#        "input": event,
+#    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+#    response = {"statusCode": 200, "body": json.dumps(body)}
 
-    return response
+#    print(response)
+
+#    return response
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+#    """
+#    return {
+#        "message": "Go Serverless v1.0! Your function executed successfully!",
+#        "event": event
     }
-    """
+#    """
+
+
+s3_client = boto3.client("s3")
+
+
+def create_thumbnail(event, context):
+    try:
+        for record in event["Records"]:
+            bucket = record["s3"]["bucket"]["name"]
+            key = unquote_plus(record["s3"]["object"]["key"])
+            print(f"The object name is {bucket}/{key}")
+    except Exception as e:
+        print(e)
